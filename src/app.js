d45075b6e42b6c0343d470a25b60082c8e6a7d22
BOMAURIEL <51271912+BOMAURIEL@users.noreply.github.com>
2025-07-26 03:22:34 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,29 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let myParagraph = document.getElementById('excuse');
+  myParagraph.innerHTML = getExcuse();
 };
+
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function getExcuse()
+{
+  let excuse_who = who[getIndex(4)];
+  let excuse_action = action[getIndex(4)];
+  let excuse_what = what[getIndex(3)];
+  let excuse_when = when[getIndex(5)];
+
+  let excuse = excuse_who + ' ' + excuse_action + ' ' + excuse_what + ' ' + excuse_when;
+  return excuse;
+}
+
+function getIndex(max)
+{
+  let num = Math.floor(Math.random() * max);
+  return num;
+}
